18c795bdb435e766b8e98d033d6a19ba7ae9f9a6
AbdallhZoair <abdallhzoair6@gmail.com>
2026-06-20 17:16:24 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ A=input("Do you want to use calculate ? (Y/N)")
 
 while (A=='Y'):
  NUM1 = input ("num1 is : ")
- op   = input ("The operator you want is (+ - / % *) : ")
+ op   = input ("The operator you want is (+ - / % * **) : ")
  NUM2 = input ("num2 is : ")
 
  if op == '+':
@@ -22,8 +22,11 @@ while (A=='Y'):
  elif op == '*':
   print ("\nthe product is : " , float(NUM1) * float(NUM2))
  
+ elif op == '**':
+  print ("\nthe power is : " , float(NUM1) ** float(NUM2))
+ 
  else:
   print ("Invalid operator. Please use + - / % *")
- A=input("Do you want to use calculate one more time? (Y/N)")
+ A=input("\nDo you want to use calculate one more time? (Y/N)")
 
-print ("i hope you have fun in this calculate (: ")
+print ("\ni hope you have fun in this calculate (: \n")
